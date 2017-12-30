b039d8d5df3410eb229b26c02889d79b1ece8db2
kolishchak <bohdan.kolishchak@gmail.com>
2017-12-30 20:22:47 +0200

    Organization project structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { ItemComponent } from './item/item.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    ItemComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule
