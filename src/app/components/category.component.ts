1b1f738c48a341a953850036b707b9a5b4c3eac6
kolishchak <bohdan.kolishchak@gmail.com>
2018-01-02 04:19:30 +0200

    Menu of categories.

--- src/app/components/category.component.ts
+++ src/app/components/category.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+import { Category } from '../interfaces/category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
-  selector: 'app-category',
+  selector: 'category',
   templateUrl: '../templates/category.component.html',
   styleUrls: ['../styles/category.component.scss']
 })
 
 export class CategoryComponent implements OnInit {
+  categories: Observable<Category[]>;
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { 
+    this.categories = this.categoryService.getCategories();
+  }
+  
+  getCategories() {
+    this.categoryService.getCategories()
+  }
 
   ngOnInit() {
+    this.getCategories()
   }
 
 }
