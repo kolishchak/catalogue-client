98d25977c0358ead0eef9fd21ba8bb277b378127
kolishchak <bohdan.kolishchak@gmail.com>
2018-01-06 04:08:52 +0200

    Reading query parameters.

--- src/app/components/category.component.ts
+++ src/app/components/category.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 
 import { Category } from '../interfaces/category';
@@ -10,19 +11,34 @@ import { CategoryService } from '../services/category.service';
   styleUrls: ['../styles/category.component.scss']
 })
 
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements OnInit, OnDestroy {
   categories: Observable<Category[]>;
+  sub: any;
+  selectedCategory: string;
+ 
 
-  constructor(private categoryService: CategoryService) { 
+  constructor(private categoryService: CategoryService,
+              private activatedRoute: ActivatedRoute) { 
     this.categories = this.categoryService.getCategories();
   }
   
   getCategories() {
-    this.categoryService.getCategories()
+    this.categories = this.categoryService.getCategories()
   }
 
   ngOnInit() {
+    this.sub = this.activatedRoute.params.subscribe(params => {
+      this.selectedCategory = params['slug'];
+    });
     this.getCategories()
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  onSelect(selectedCategory: string) {
+    this.selectedCategory = selectedCategory;
   }
 
 }
