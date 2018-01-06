94e9536f1a9d1fefc2d2a210932b1426ba278c54
kolishchak <bohdan.kolishchak@gmail.com>
2018-01-06 04:30:08 +0200

    Property changes detection.

--- src/app/components/item.component.ts
+++ src/app/components/item.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChange } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
 import { Item } from '../interfaces/item';
 import { ItemService } from '../services/item.service';
-
 
 @Component({
   selector: 'item',
@@ -11,17 +10,23 @@ import { ItemService } from '../services/item.service';
   styleUrls: ['../styles/item.component.scss'],
 })
 
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
   @Input() slug: string;
 
   items: Observable<Item[]>;
   p: number = 1;
 
-  constructor(private itemService: ItemService) { 
-  }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit() {
     this.items = this.itemService.getItems(this.slug, this.p.toString())
+  }
+
+  ngOnChanges(changes: { [property_name: string]: SimpleChange }) {
+    for (let propName in changes) {
+      let chng = changes[propName];
+      this.items = this.itemService.getItems(chng.currentValue, this.p.toString())
+    }
   }
 
   getPage(page: number) {
